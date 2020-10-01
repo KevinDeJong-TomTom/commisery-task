b42bdc14f075b32e0dfe253d4c0f8ed89b4247c9
Kevin de Jong (EHV) <kevin.dejong@tomtom.com>
2020-10-01 10:22:51 +0200

    feet: incorrect commit

--- commiseryTask/index.js
+++ commiseryTask/index.js
@@ -1,9 +1,24 @@
 const { Octokit } = require("@octokit/rest");
 const octokit = new Octokit();
 
-// Compare: https://docs.github.com/en/rest/reference/repos/#list-organization-repositories
-const { data: pullRequest } = await octokit.pulls.get({
-    owner: "KevinDeJong-TomTom",
-    repo: "commisery-task",
-    pull_number: 1,
-  });
+var tl = require('azure-pipelines-task-lib/task')
+
+async function run() {
+    const { data: pullRequest } = await octokit.pulls.get({
+        owner: "KevinDeJong-TomTom",
+        repo: "commisery-task",
+        pull_number: 1,
+    });
+
+    const { data: commits } = await octokit.pulls.listCommits({
+        owner: "KevinDeJong-TomTom",
+        repo: "commisery-task",
+        pull_number: 1,
+    });
+
+    await tl.tool('commisery-verify-msg').line(commits[0]['sha']).exec()
+
+    console.log(commits)
+}
+
+run()
